a181a21ecd03728e8cf39b8d4c36ab59a616f455
青空 <1057605824@qq.com>
2020-06-11 19:51:31 +0800

    添加类型

--- src/App.vue
+++ src/App.vue
@@ -80,42 +80,85 @@ export default {
 
             this.myCharts.setOption(newData);
         },
-        // 柱形图
+        // 线形图
         lineCharts() {
             return {
-                title: {
-                    text: this.chart.title,
-                },
+                title: { text: "Line Chart" },
                 tooltip: {},
-                legend: {
-                    data: ["销量"],
+                toolbox: {
+                    feature: {
+                        dataView: {},
+                        saveAsImage: {
+                            pixelRatio: 2,
+                        },
+                        restore: {},
+                    },
                 },
-                xAxis: {
-                    data: [
-                        "衬衫",
-                        "羊毛衫",
-                        "雪纺衫",
-                        "裤子",
-                        "高跟鞋",
-                        "袜子",
-                    ],
-                },
+                xAxis: {},
                 yAxis: {},
                 series: [
                     {
-                        name: "销量",
-                        type: "bar",
-                        data: [5, 20, 36, 10, 10, 20],
+                        type: "line",
+                        smooth: true,
+                        data: [
+                            [12, 5],
+                            [24, 20],
+                            [36, 36],
+                            [48, 10],
+                            [60, 10],
+                            [72, 20],
+                        ],
                     },
                 ],
             };
         },
         // 饼图
         pieCharts() {
-           
+            return {
+                series: {
+                    type: "pie",
+                    // roseType: "angle",
+                    itemStyle: {
+                        shadowBlur: 200,
+                        shadowOffsetX: 0,
+                        shadowOffsetY: 10,
+                        showdowColor: "#000",
+                    },
+                    data: [
+                        { name: "HTML", value: 1212 },
+                        { name: "CSS", value: 2323 },
+                        { name: "JS", value: 1919 },
+                    ],
+                },
+            };
         },
+        // 柱形图
         barCharts() {
-            
+            return {
+                title: {
+                    text: "ECharts 入门示例",
+                },
+                tooltip: {},
+                legend: {
+                    data: ["销量", "老婆"],
+                },
+                xAxis: {},
+                yAxis: {
+                    data: ["衬衫", "羊毛衫"],
+                },
+                series: [
+                    {
+                        name: "销量",
+                        type: "bar",
+                        data: [5, 20, 36, 10, 10, 20],
+                    },
+                    {
+                        name: "老婆",
+                        type: "bar",
+                        data: [5, 20, 36, 10, 10, 20],
+                    },
+                ],
+            };
         },
     },
     mounted() {
